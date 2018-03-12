da9a913098ced71cf442a0313b7715f48af5d90f
JD Marymee <jmarymee@microsoft.com>
2018-03-12 11:44:23 -0700

    Removed Storage String

--- BlobUploaderNET/Global.asax.cs
+++ BlobUploaderNET/Global.asax.cs
@@ -15,13 +15,6 @@ namespace BlobUploaderNET
     {
         protected void Application_Start()
         {
-            //"DefaultEndpointsProtocol=https;AccountName=jdmsparktest;AccountKey=Jv7s93MYpjrNe64ohoe9OfZfyPQdRmKKmqtgwbT918ct0YCE5UW2yopGYS7RWo9f+3o35iQBMExgIEuCGFNKtg==;EndpointSuffix=core.windows.net"
-
-            //string dirName = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-
-            //string jsoncConfig = System.IO.File.ReadAllText("credentials.json");
-            //string jsoncConfig = System.IO.Directory.GetCurrentDirectory();
-
             string path = AppDomain.CurrentDomain.BaseDirectory;
             string conn = "";
 
@@ -39,12 +32,17 @@ namespace BlobUploaderNET
                 }
             }
 
-
             Application["StorageConnString"] = conn;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+
         }
     }
 }
